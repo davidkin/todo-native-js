f18afe14ed277ff233c90acd35db05a24d297a21
David <davidosimo1@gmail.com>
2019-08-21 14:57:54 +0300

    - add functionality for make finishTask; - add functionality for make unfinishTask; - add functionality for malke editTask;

--- js/index.js
+++ js/index.js
@@ -3,13 +3,22 @@ const inputTask = document.querySelector('.new-task');
 const taskList = document.querySelector('.unfinished-tasks');
 const finishedTasks = document.querySelector('.finished-tasks');
 
+let editMode = false;
+
 function createNewTask(taskName) {
     const task = document.createElement('li');
-    task.className = 'list-group-item my-list-item';
+    task.className = 'list-group-item';
     task.innerHTML = `
+        <div class="checkbox-block">
+            <input type="checkbox" class="ready" id="checkbox-ready">
+        </div>  
+        
         <div class="info-block">
-            <input type="checkbox" class="ready">
-            <p class="task-name">${taskName}</p>
+            <label for="checkbox-ready" class="task-name">${taskName}</label>
+            <input type="text" 
+                    class="form-control edit-input" 
+                    value="${taskName}"
+            >
         </div>
 
         <div class="buttons-block">
@@ -38,7 +47,36 @@ function addTask() {
 }
 
 function editTask() {
-    console.log('---', 'editTask');
+    editMode = !editMode;
+
+    const button = this;
+    const btnBlock = this.parentNode;
+    const list = btnBlock.parentNode;
+    const checkboxBlock = list.querySelector('.checkbox-block');
+    const taskName = list.querySelector('.task-name');
+    const input = list.querySelector('.edit-input');
+    
+    if (editMode) {
+        list.classList.add('my-list-item');
+
+        input.style.display = 'block';
+        taskName.style.display = 'none';
+        checkboxBlock.style.display = 'none';
+
+        button.innerHTML = '<i class="fas fa-save"></i>'
+
+    } else {
+        taskName.innerText = input.value;
+
+        list.classList.remove('my-list-item');
+
+        input.style.display = 'none';
+        taskName.style.display = 'block';
+        checkboxBlock.style.display = 'block';
+
+        button.innerHTML = '<i class="fas fa-edit"></i>'
+
+    }
 }
 
 function deleteTask() {
@@ -47,16 +85,23 @@ function deleteTask() {
     const list = task.parentNode;
 
     list.removeChild(task);
-
-    console.log('---', 'deleteTask' );
 }
 
 function finishTask() {
-    console.log('---', 'finishTask');
+    const infoBlock = this.parentNode;
+    const list = infoBlock.parentNode;
+
+    finishedTasks.appendChild(list);
+    bindTasksEvent(list, unfinishTask);
+    
 }
 
 function unfinishTask() {
-    console.log('---', 'unfinishTask');
+    const infoBlock = this.parentNode;
+    const list = infoBlock.parentNode;
+
+    taskList.appendChild(list);
+    bindTasksEvent(list, finishTask);
 }
 
 function bindTasksEvent(task, checkboxEvent) {
