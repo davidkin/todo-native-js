7405ec75d63162389bde54e15acbe81d8db52e29
David <davidosimo1@gmail.com>
2019-08-21 15:51:35 +0300

    - add functionality for creating project; - code refactoring

--- js/index.js
+++ js/index.js
@@ -1,9 +1,95 @@
-const addButton = document.querySelector('.add-task');
-const inputTask = document.querySelector('.new-task');
-const taskList = document.querySelector('.unfinished-tasks');
-const finishedTasks = document.querySelector('.finished-tasks');
+const projectsBlock = document.querySelector('.projects');
+const addProjectButton = document.querySelector('.add-project');
+
+let addTaskButton = null;
+let projectDeleteButton = null;
+
+let inputAddTask = null;
+let taskList = null;
+let finishedTasksList = null;
 
 let editMode = false;
+
+// ------------------ Create Project ------------------
+
+function createNewProject (projectName) {
+    const project = document.createElement('div');
+    project.className = 'todo';
+    project.innerHTML = `
+        <div class="todo-title-block">
+            <h4 class="todo-title">${projectName}</h4>
+            <div class="title-buttons-block">
+                <a href="javascript:void(0);" class="icon-btn project-edit">
+                    <i class="fas fa-edit"></i>
+                </a>
+                <a href="javascript:void(0);" class="icon-btn project-delete">
+                    <i class="fas fa-trash"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="todo-content">
+            <div class="todo-header">
+                <h3>Add task</h3>
+                <div class="todo-header-info mb-2">
+                    <div class="form-group new-task-block">
+                        <input type="text" class="form-control new-task" placeholder="write new task">
+                    </div>
+                    <button class="btn btn-primary add-task">Add Task</button>
+                </div>
+            </div>
+
+            <div class="todo-body">
+                <div class="block block-border">
+                    <h3 class="tasks-title">List of tasks</h3>
+                    <ul class="list-group mb-2 unfinished-tasks"></ul>
+                </div>
+                
+                <div class="block">
+                    <h3 class="tasks-title">Finished tasks</h3>
+                    <ul class="list-group mb-2 finished-tasks"></ul>
+                </div>
+            </div>
+        </div>
+    `;
+
+    return project;
+}
+
+function addProject(title = 'My Project') {
+    const project = createNewProject(title);
+
+    projectsBlock.appendChild(project);
+}
+
+addProjectButton.addEventListener('click', () => {
+    const createProjectInput = document.querySelector('.create-project-input');
+
+    addProject(createProjectInput.value);
+
+    createProjectInput.value = '';
+    
+    addTaskButton = document.querySelector('.add-task');
+    inputAddTask = document.querySelector('.new-task');
+    taskList = document.querySelector('.unfinished-tasks');
+    finishedTasksList = document.querySelector('.finished-tasks');
+    projectDeleteButton = document.querySelector('.project-delete');
+
+    addTaskButton.addEventListener('click', addTask);
+    projectDeleteButton.addEventListener('click', deleteProject);
+});
+
+// ------------------ Create Events Functions For Project ------------------
+
+function deleteProject() {
+    const btnBlock = this.parentNode;
+    const titleBlock = btnBlock.parentNode;
+    const project = titleBlock.parentNode;
+
+    projectsBlock.removeChild(project);
+}
+
+// ------------------ Create Task ------------------
 
 function createNewTask(taskName) {
     const task = document.createElement('li');
@@ -34,17 +120,19 @@ function createNewTask(taskName) {
 }
 
 function addTask() {
-    if (!inputTask.value) {
+    if (!inputAddTask.value) {
         return;
     }
 
-    const task = createNewTask(inputTask.value);
+    const task = createNewTask(inputAddTask.value);
     taskList.appendChild(task);
 
     bindTasksEvent(task, finishTask)
     
-    inputTask.value = '';
+    inputAddTask.value = '';
 }
+
+// ------------------ Create Events Functions For Tasks ------------------
 
 function editTask() {
     editMode = !editMode;
@@ -64,7 +152,6 @@ function editTask() {
         checkboxBlock.style.display = 'none';
 
         button.innerHTML = '<i class="fas fa-save"></i>'
-
     } else {
         taskName.innerText = input.value;
 
@@ -75,25 +162,22 @@ function editTask() {
         checkboxBlock.style.display = 'block';
 
         button.innerHTML = '<i class="fas fa-edit"></i>'
-
     }
 }
 
 function deleteTask() {
     const btnBlock = this.parentNode;
     const task = btnBlock.parentNode;
-    const list = task.parentNode;
 
-    list.removeChild(task);
+    taskList.removeChild(task);
 }
 
 function finishTask() {
     const infoBlock = this.parentNode;
     const list = infoBlock.parentNode;
 
-    finishedTasks.appendChild(list);
+    finishedTasksList.appendChild(list);
     bindTasksEvent(list, unfinishTask);
-    
 }
 
 function unfinishTask() {
@@ -113,5 +197,3 @@ function bindTasksEvent(task, checkboxEvent) {
     editButton.onclick = editTask;
     deleteButton.onclick = deleteTask;
 }
-
-addButton.addEventListener('click', addTask);
